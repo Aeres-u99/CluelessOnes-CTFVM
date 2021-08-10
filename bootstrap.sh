da76cb01000b4d2639a29ba29dd2c53e245e0be5
Akuma <aeres99@hotmail.com>
2021-08-10 23:44:52 +0000

    New changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,22 @@
 # Importing Keys
 wget -q -O - https://archive.kali.org/archive-key.asc | sudo apt-key add
+echo "[*] + Updating and Upgrading"
 sudo apt-get update && sudo apt-get upgrade -y
 cd /home/vagrant
 sudo apt-get update --fix-missing
-sudo apt-get upgrade --all -y
+sudo apt-get upgrade -y
 # Setting up stuffs
+echo "[*] Installing Basic Tools"
 sudo apt install curl wget git zsh python3-pip tmux neovim vim gpg -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "[*] Installing p10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i.bak "s/\"robbyrussell\"/\"powerlevel10k\/powerlevel10k\"/" .zshrc
+echo "[*] Fetching tmux config"
 cd /home/vagrant ; wget -c https://raw.githubusercontent.com/Aeres-u99/CluelessOnes-CTFVM/main/.tmux.conf
 # sudo chsh
 # Installing the tools
+echo "[*] Installing hacking tools"
 sudo apt install radare2 john binwalk nmap sublist3r gobuster gdb hashcat bettercap exif python3-venv -y
 wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
 echo source ~/.gdbinit-gef.py >> ~/.gdbinit
